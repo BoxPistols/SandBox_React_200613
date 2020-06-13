2d585b2b84bf3ed4a2963abf992473350bc2c3f8
A_ito <BoxPistols@users.noreply.github.com>
2020-06-13 22:31:49 +0900

    functional Component

--- src/Comp01.jsx
+++ src/Comp01.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
+const msg = {
+  fontSize: "2rem",
+  color: "teal"
+};
+// functional Component
+function Welcome(props) {
+  return <p style={msg}>Welcome,{props.name}</p>;
+}
+
+function Calc(props) {
+  let total = 0;
+  for (let i = 0; i <= props.number; i++) {
+    total += i;
+  }
+  return (
+    <p>
+      1から{props.number}までの合計は{total}です
+    </p>
+  );
+}
+
 export default function Comp01() {
   const on = () => {
     alert("Action!");
@@ -7,6 +28,9 @@ export default function Comp01() {
 
   return (
     <>
+      <Calc number="3" />
+      <Calc number="4" />
+      <Welcome name="Taro" />
       <p onClick={on}>Comp01</p>
     </>
   );
